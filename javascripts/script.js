553328356d7f83d96778ef537121eda364935331
bahati patrick <bahatipatrick05@gmail.com>
2023-11-29 21:31:05 +0200

    added feature to sort tasks by importance, alphabetically, date and by completion

--- javascripts/script.js
+++ javascripts/script.js
@@ -6,15 +6,24 @@ const completedTasks = document.querySelector('#completed-tasks');
 const remainingTasks = document.querySelector('#remaining-tasks');
 const importantTasks = document.querySelector('#important-tasks');
 const mainInput = document.querySelector('#todo-form input');
+const clearSortlink = document.getElementById('clear-sorts');
 
 let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
 // get all tasks
-if (localStorage.getItem('tasks')) {
-    tasks.sort((a,b) => a.id - b.id ).map((task) => {
-        createTask(task);
-    });
+function getAllTasks() {
+    if (localStorage.getItem('tasks')) {
+        tasks.sort((a,b) => a.id - b.id ).map((task) => {
+            createTask(task);
+            if (task.isImportant){
+                changeMarked(task.id, task);
+            }        
+        });
+        countTasks()
+    }
 }
+
+getAllTasks();
 
 todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -107,7 +116,6 @@ function createTask(task){
     // get task id and mark it as important
     const markButton = taskEl.querySelector('.mark-task');
     markButton.addEventListener('click', (e)=>{
-        // const taskId = e.target.closest('li').id;
         markAsImportant(task.id)
     }); 
 }
@@ -165,26 +173,89 @@ function updateTask(taskId, el) {
     countTasks()
 }
 
-// mark task as important
+// mark / unmark task as important
 function markAsImportant(taskId) {
     const task = tasks.find((task) => task.id === parseInt(taskId));
     task.isImportant = !task.isImportant;
 
+    changeMarked(taskId, task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    countTasks();
+}
+
+// change style of marked / unmarked task
+function changeMarked(taskId, task){
     const taskEl = document.getElementById(taskId);
     const taskElMarkButtonSpan = taskEl.childNodes[3].childNodes[1].childNodes[1];
     const taskElMarkButton = taskEl.childNodes[3].childNodes[1];
 
     if (task.isImportant) {
-        taskEl.classList.add('important');
+        taskEl.classList.toggle('important');
         taskElMarkButtonSpan.textContent = 'Unmark'
         taskElMarkButton.setAttribute('title','Unmark as Important');
     } else {
-        taskEl.classList.remove('important');
+        taskEl.classList.toggle('important');
         taskElMarkButtonSpan.textContent = 'Mark'
         taskElMarkButton.setAttribute('title','Mark as Important');
     }
+}
 
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+// ================================================================================================================================================
+// sorting functionality
 
+// Get all the sort buttons by their class name
+const sortLinks = document.getElementsByClassName('sort_link');
+
+// Loop through each sort button and add an event listener
+for (let link of sortLinks) {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    let sortOption = link.id.split('-')[2];
+    sortTasks(sortOption);
+    clearSortlink.classList.add('clear_sorts');
+  });
+}
+
+// sort function 
+function sortTasks(sortOption) {
+  let allOptions = {
+    importance: (a, b) => a.isImportant - b.isImportant,
+    ascending: (a, b) => b.name.localeCompare(a.name),
+    descending: (a, b) => a.name.localeCompare(b.name),
+    old: (a, b) => b.id - a.id,
+    new: (a, b) => a.id - b.id,
+    completion: (a, b) => a.isCompleted - b.isCompleted
+  };
+
+  tasks.sort(allOptions[sortOption]);
+  // Update the tasks order on the page after sorting
+  updateTasksOrder();
+}
+
+// update tasks order on screen 
+function updateTasksOrder() {
+  todoList.innerHTML = '';
+
+  tasks.forEach((task) => {
+    createTask(task);
+    if (task.isImportant){
+      changeMarked(task.id, task);
+    }  
+    countTasks();
+  });
+}
+
+// get the clear sorting link
+clearSortlink.addEventListener('click', () => {
+    clearSorting();
+    clearSortlink.classList.remove('clear_sorts');
+});
+
+// clear sort function
+function clearSorting() {    
+    todoList.innerHTML = '';
+    getAllTasks();
     countTasks();
 }
